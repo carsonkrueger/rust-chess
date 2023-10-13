31811ca3499f2d79c13013704d7ed47ed2c0c09d
carsonkrueger <crkruege@gmail.com>
2023-10-12 19:55:37 -0600

    chess board image loading correctly

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use druid::{
-    widget::{Button, Flex, Label},
-    AppLauncher, Data, Widget, WindowDesc,
+    widget::{Align, Button, Flex, Image, Label},
+    AppLauncher, Data, ImageBuf, Widget, WindowDesc,
 };
+use image::open;
 
 fn main() {
-    let window = WindowDesc::new(build_root());
+    let window = WindowDesc::new(build_board());
 
     let initial_state = ScreenState {
         title: String::from("Main Menu"),
@@ -20,12 +21,22 @@ struct ScreenState {
     title: String,
 }
 
-fn build_root() -> impl Widget<ScreenState> {
-    let label = Label::new("Rust Chess");
+fn build_board() -> impl Widget<ScreenState> {
+    let board_file_path: String = String::from("assets/chessboard.jpg");
 
-    let play_btn = Button::new("Play").on_click(|_, _, _| println!("You clicked play"));
+    let img = open(board_file_path).unwrap();
+    let w = img.width();
+    let h = img.height();
 
-    let layout = Flex::column().with_child(label).with_child(play_btn);
+    let img_slc = img.as_bytes();
+    let arc_slc: std::sync::Arc<[u8]> = std::sync::Arc::from(img_slc);
 
-    layout
+    let img_buf = ImageBuf::from_raw(
+        arc_slc,
+        druid::piet::ImageFormat::Rgb,
+        w as usize,
+        h as usize,
+    );
+
+    Image::new(img_buf)
 }
